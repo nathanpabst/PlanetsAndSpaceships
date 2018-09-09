4fcbedd6bed47dc4f1804ec52fcff42598ab044d
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-09 15:09:23 -0500

    Insert method working

--- PlanetsAndSpaceships/Program.cs
+++ PlanetsAndSpaceships/Program.cs
@@ -7,14 +7,17 @@ namespace PlanetsAndSpaceships
     {
         static void Main(string[] args)
         {
+            //order of planets: Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
             List<string> planetList = new List<string>() { "Mercury", "Mars" };
             planetList.Add("Jupiter");
             planetList.Add("Saturn");
 
             List<string> morePlanets = new List<string>() { "Uranus", "Neptune" };
 
+            //AddRange() adds the planets listed in morePlanets to the end of planetList
             planetList.AddRange(morePlanets);
 
+            planetList.Insert(2, "Earth");
 
 
             Console.ReadLine();
