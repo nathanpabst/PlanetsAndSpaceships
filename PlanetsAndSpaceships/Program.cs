4036e9d9b02a5451dc0aa081d84c89a96769899a
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-09 15:39:32 -0500

    GetRange and Remove methods working

--- PlanetsAndSpaceships/Program.cs
+++ PlanetsAndSpaceships/Program.cs
@@ -17,7 +17,17 @@ namespace PlanetsAndSpaceships
             //AddRange() adds the planets listed in morePlanets to the end of planetList
             planetList.AddRange(morePlanets);
 
+            //Insert() returns a new string in which a specified string is inserted at a specified index position in the current string instance.
             planetList.Insert(2, "Earth");
+            planetList.Insert(1, "Venus");
+
+            planetList.Add("Pluto");
+
+            //GetRange() creates a shallow copy of a range of elements frm the planetList List. The parameters tell the method the index value of where to begin and how many to include.
+            List<string> rockyPlanets = new List<string>();
+            rockyPlanets = planetList.GetRange(0, 4);
+
+            planetList.Remove("Pluto");
 
 
             Console.ReadLine();
